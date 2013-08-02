7aa93768c43b961992bb9cb264c145d1edf83b9b
GreatEagle0303 <greateagle0303@gmail.com>
2013-08-02 11:40:01 -0500

    Adding more tests for silence to Bob

--- assignments/ruby/rna-transcription/example.rb
+++ assignments/ruby/rna-transcription/example.rb
@@ -1,4 +1,17 @@
-class NucleicAcid
+# In general, inheriting from Ruby core classes
+# is dangerous and confusing.
+# http://words.steveklabnik.com/beware-subclassing-ruby-core-classes
+# In this case it works out with no surprises.
+class DNA < String
+  THYMIDINE = 'T'
+  URACIL = 'U'
+
+  def to_rna
+    tr THYMIDINE, URACIL
+  end
+end
+
+class DeoxyribonucleicAcid
   THYMIDINE = 'T'
   URACIL = 'U'
 
@@ -7,16 +20,8 @@ class NucleicAcid
     @strand = strand
   end
 
-  require 'forwardable'
-  extend Forwardable
-  def_delegators :strand, :to_str, :to_s, :==
-end
-
-class RibonucleicAcid < NucleicAcid
-end
-
-class DeoxyribonucleicAcid < NucleicAcid
   def to_rna
-    RibonucleicAcid.new(strand.tr THYMIDINE, URACIL)
+    strand.tr THYMIDINE, URACIL
   end
 end
+
