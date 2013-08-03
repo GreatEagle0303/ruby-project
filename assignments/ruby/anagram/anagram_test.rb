cf9f319a6ae045ddf405bf943edd594c65ff22db
GreatEagle0303 <greateagle0303@gmail.com>
2013-08-03 12:03:56 -0600

    Change instance variable to be 'source'. Phrase.phrase look weird.

--- assignments/ruby/anagram/anagram_test.rb
+++ assignments/ruby/anagram/anagram_test.rb
@@ -25,13 +25,7 @@ class AnagramTest < MiniTest::Unit::TestCase
   def test_does_not_confuse_different_duplicates
     skip
     detector = Anagram.new('abb')
-    assert_equal [], detector.match(['aab'])
-  end
-
-  def test_eliminate_anagrams_with_same_checksum
-    skip
-    detector = Anagram.new('abb')
-    assert_equal [], detector.match(['aac'])
+    assert_equal [], detector.match(['baa'])
   end
 
   def test_detect_anagram
