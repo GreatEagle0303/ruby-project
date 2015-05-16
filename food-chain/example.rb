fd3ff8e897c762fe622f0fce092264e6d2cc1f6b
GreatEagle0303 <greateagle0303@gmail.com>
2015-05-15 20:02:40 -0600

    add a getting started to hello-world

--- food-chain/example.rb
+++ food-chain/example.rb
@@ -1,83 +1,67 @@
-module FoodChain
-  VERSION = 1
-
-  def self.song
+class FoodChainSong
+  def sing
     verses(1, 8)
   end
 
-  def self.verses(lower_bound, upper_bound)
-    (lower_bound..upper_bound).map { |i| verse(i) }.join("\n")
+  def verses(first, last)
+    first.upto(last).map do |number|
+      verse(number)
+    end.join("\n") + "\n"
   end
 
-  def self.verse(number)
+  def verse(number)
     case number
     when 1
-      <<-LINES
-I know an old lady who swallowed a fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-      LINES
+      "I know an old lady who swallowed a fly.\n" \
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
     when 2
-      <<-LINES
-I know an old lady who swallowed a spider.
-It wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-      LINES
+      "I know an old lady who swallowed a spider.\n" \
+      "It wriggled and jiggled and tickled inside her.\n" \
+      "She swallowed the spider to catch the fly.\n" \
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
     when 3
-      <<-LINES
-I know an old lady who swallowed a bird.
-How absurd to swallow a bird!
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-      LINES
+      "I know an old lady who swallowed a bird.\n" \
+      "How absurd to swallow a bird!\n" \
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
+      "She swallowed the spider to catch the fly.\n" \
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
     when 4
-      <<-LINES
-I know an old lady who swallowed a cat.
-Imagine that, to swallow a cat!
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-      LINES
+      "I know an old lady who swallowed a cat.\n" \
+      "Imagine that, to swallow a cat!\n" \
+      "She swallowed the cat to catch the bird.\n" \
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
+      "She swallowed the spider to catch the fly.\n" \
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
     when 5
-      <<-LINES
-I know an old lady who swallowed a dog.
-What a hog, to swallow a dog!
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-      LINES
+      "I know an old lady who swallowed a dog.\n" \
+      "What a hog, to swallow a dog!\n" \
+      "She swallowed the dog to catch the cat.\n" \
+      "She swallowed the cat to catch the bird.\n" \
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
+      "She swallowed the spider to catch the fly.\n" \
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
     when 6
-      <<-LINES
-I know an old lady who swallowed a goat.
-Just opened her throat and swallowed a goat!
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-      LINES
+      "I know an old lady who swallowed a goat.\n" \
+      "Just opened her throat and swallowed a goat!\n" \
+      "She swallowed the goat to catch the dog.\n" \
+      "She swallowed the dog to catch the cat.\n" \
+      "She swallowed the cat to catch the bird.\n" \
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
+      "She swallowed the spider to catch the fly.\n" \
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
     when 7
-      <<-LINES
-I know an old lady who swallowed a cow.
-I don't know how she swallowed a cow!
-She swallowed the cow to catch the goat.
-She swallowed the goat to catch the dog.
-She swallowed the dog to catch the cat.
-She swallowed the cat to catch the bird.
-She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.
-She swallowed the spider to catch the fly.
-I don't know why she swallowed the fly. Perhaps she'll die.
-      LINES
+      "I know an old lady who swallowed a cow.\n" \
+      "I don't know how she swallowed a cow!\n" \
+      "She swallowed the cow to catch the goat.\n" \
+      "She swallowed the goat to catch the dog.\n" \
+      "She swallowed the dog to catch the cat.\n" \
+      "She swallowed the cat to catch the bird.\n" \
+      "She swallowed the bird to catch the spider that wriggled and jiggled and tickled inside her.\n" \
+      "She swallowed the spider to catch the fly.\n" \
+      "I don't know why she swallowed the fly. Perhaps she'll die.\n"
     when 8
-      <<-LINES
-I know an old lady who swallowed a horse.
-She's dead, of course!
-      LINES
+      "I know an old lady who swallowed a horse.\n" \
+      "She's dead, of course!\n"
     end
   end
 end
