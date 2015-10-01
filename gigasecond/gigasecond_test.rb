cf22a61b4d2efb47efaf0d495ca8f5a5385f82be
GreatEagle0303 <greateagle0303@gmail.com>
2015-10-01 08:40:29 -0700

    Update binary version test comments to be more clear for user

--- gigasecond/gigasecond_test.rb
+++ gigasecond/gigasecond_test.rb
@@ -43,14 +43,8 @@ class GigasecondTest < Minitest::Test
     skip
   end
 
-  # Problems in exercism evolve over time,
-  # as we find better ways to ask questions.
-  # The version number refers to the version of the problem you solved,
-  # not your solution.
-  #
-  # Define a constant named VERSION inside of Gigasecond.
-  # If you're curious, read more about constants on RubyDoc:
-  # http://ruby-doc.org/docs/ruby-doc-bundle/UsersGuide/rg/constants.html
+  # This test is for the sake of people providing feedback, so they
+  # know which version of the exercise you are solving.
   def test_bookkeeping
     assert_equal 1, Gigasecond::VERSION
   end
