e10f656a4449929e4a9669a2ff4c8eb80fb60d4b
GreatEagle0303 <greateagle0303@gmail.com>
2017-05-03 13:05:50 +0100

    Remove redundant gem 'minitest', '>= 5.0.0' line.

--- exercises/phone-number/example.rb
+++ exercises/phone-number/example.rb
@@ -1,29 +1,45 @@
-module PhoneNumber
-  NANP_PROTOCOL = %r{
-    \A
-    (?<international_code>1?)
-    (?<area_code>[2-9]{1}[0-9]{2})
-    (?<exchange_code>[2-9]{1}[0-9]{2})
-    (?<subscriber>[0-9]{4})
-    \z
-  }x
-
-  def self.clean(number)
-    sanitized = digits_only(number)
-    sections = nanp_parse(sanitized)
-    format '%<area_code>s%<exchange_code>s%<subscriber>s', sections if sections
+class PhoneNumber
+  attr_reader :number
+  def initialize(number)
+    @number = clean(number)
   end
 
-  def self.digits_only(number)
-    number.gsub(/\D/, '')
+  def area_code
+    number[0..2]
   end
 
-  def self.nanp_parse(number)
-    matches = number.match(NANP_PROTOCOL)
-    matches.names.map(&:to_sym).zip(matches.captures).to_h if matches
+  # technically, the central office (exchange) code
+  def exchange_code
+    number[3..5]
   end
-end
 
-module BookKeeping
-  VERSION = 1
+  def subscriber_number
+    number[6..9]
+  end
+
+  def to_s
+    "(#{area_code}) #{exchange_code}-#{subscriber_number}"
+  end
+
+  private
+
+  def clean(number)
+    return '0' * 10 if number.match(/[a-zA-Z]/)
+    number = number.gsub(/[^0-9]/, '')
+    normalize(number)
+  end
+
+  def normalize(number)
+    if valid?(number)
+      number[/(\d{10})\z/, 1]
+    else
+      '0' * 10
+    end
+  end
+
+  def valid?(number)
+    return true if number.length == 10
+    return true if number.length == 11 && number.start_with?('1')
+    false
+  end
 end
