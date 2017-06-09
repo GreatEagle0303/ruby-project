fbb2be748d53872a0c598aa7e9be0e1fd4bbb53c
GreatEagle0303 <greateagle0303@gmail.com>
2017-06-09 11:13:16 -0600

    Rename the global exercise hints file

--- exercises/bob/.meta/generator/bob_case.rb
+++
@@ -1,9 +0,0 @@
-require 'generator/exercise_case'
-
-class BobCase < Generator::ExerciseCase
-  def workload
-    indent_lines(["remark = #{input.inspect}",
-      "assert_equal '#{expected}', Bob.hey(remark), %q{Bob hears #{input.inspect}, and..}"
-    ], 4)
-  end
-end
