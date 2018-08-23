52c84681f5898ee1a9a97ee44a448a7dc0a82640
GreatEagle0303 <greateagle0303@gmail.com>
2018-08-23 13:15:16 -0700

    Update rna transcription tests to expect ArgumentError if invalid input is supplied

--- exercises/transpose/.meta/solutions/transpose.rb
+++ exercises/transpose/.meta/solutions/transpose.rb
@@ -1,17 +1,10 @@
 class Transpose
   def self.transpose(input)
     lines = input.split("\n")
-    longest = lines.map(&:length).max || 0
+    max_line_length = lines.map(&:length).max
 
-    padding_array = [nil] * longest
-    equal_length_arrays = lines.map { |line| (line.chars + padding_array).take(longest) }
-    # Transpose.
-    transposed = equal_length_arrays.transpose
-    # strip trailing nils (remove right padding)
-    no_trailing_nils = transposed.map { |row| row.reverse.drop_while(&:nil?).reverse }
-    # replace remaining nils with spaces (add left padding)
-    correctly_padded = no_trailing_nils.map { |row| row.map { |c| c.nil? ? ' ' : c } }
-    # turn it back into a string
-    correctly_padded.map(&:join).join("\n")
+    lines
+      .map { |line| line.ljust(max_line_length).chars }
+      .transpose.map(&:join).join("\n").strip
   end
 end
