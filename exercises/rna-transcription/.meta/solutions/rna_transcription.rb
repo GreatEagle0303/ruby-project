015f9cffe7b034b2cd969a5a4cf77f5829b84de7
GreatEagle0303 <greateagle0303@gmail.com>
2018-08-26 08:06:33 +0100

    Add helper accessors for canonical data values

--- exercises/rna-transcription/.meta/solutions/rna_transcription.rb
+++ exercises/rna-transcription/.meta/solutions/rna_transcription.rb
@@ -1,6 +1,5 @@
 module Complement
   def self.of_dna(strand)
-    raise ArgumentError if strand =~ /[^CGTA]/
-    strand.tr('CGTA', 'GCAU')
+    strand =~ /[^CGTA]/ ? '' : strand.tr('CGTA', 'GCAU')
   end
 end
